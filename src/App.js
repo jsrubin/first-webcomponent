99f9f51d406ab97f91fcb86dbd7c0a9e49520cf7
jsrubin <justin.rubin@gmail.com>
2021-10-07 06:24:04 -0700

    add geolocation and local weather

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 //import our Web Component
 import "./web-components/search-result";
+import "./web-components/weather-card";
+
+const Loading = () => {
+  return <div className="loader"></div>;
+};
+
+const getGeoLocation = callback => {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let lat = position.coords.latitude.toFixed(7);
+    let long = position.coords.longitude.toFixed(7);
+    callback({ latitude: lat, longitude: long });
+  });
+};
 
 function App() {
   const [name, setName] = useState("");
+  const [geolocation, setGeolocation] = useState("");
+
+  useEffect(() => {
+    getGeoLocation(setGeolocation);
+  }, []);
+
+  useEffect(() => {
+    if (geolocation && geolocation.latitude) {
+      console.log(
+        ` did we find lat long?? ${geolocation.latitude} ${geolocation.longitude}`
+      );
+    }
+  }, [geolocation]);
 
   return (
     <div className="App">
       <header className="App-header">
+        {geolocation && geolocation.latitude && geolocation.longitude ? (
+          <weather-card
+            latitude={geolocation.latitude || ""}
+            longitude={geolocation.longitude || ""}
+          ></weather-card>
+        ) : (
+          <>
+            <Loading></Loading>
+            <p />
+          </>
+        )}
         <input
           placeholder="Enter your name"
           onChange={event => setName(event.target.value)}
           value={name}
         ></input>
 
-        <div class="greeting">Hello {name}!</div>
+        <div className="greeting">Hello {name}!</div>
 
         <search-result name-attribute={name}></search-result>
       </header>
